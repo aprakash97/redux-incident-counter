f7e6d74626e3fbcc497c1fc0cefcb3ade78094dc
aprakash97 <prakashakrakr@gmail.com>
2023-05-01 19:10:14 +0530

    last

--- src/Counter.js
+++ src/Counter.js
@@ -3,18 +3,9 @@ import { increment, INCREMENT, DECREMENT, decrement, set } from './actions';
 
 export const Counter = () => {
   const incident = 'Incident';
-  
+
   let count = useSelector((state) => state.count);
   const dispatch = useDispatch();
-
-  // const increment = () => {
-  //   count++;
-  //   console.log(count);
-  // };
-  // function handleChange(event) {
-  //   console.log(event.target.value);
-  // }
-  // console.log('Testing');
 
   return (
     <main className="Counter">
